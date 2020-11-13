1fbb60d51a77a52f25d8e5de0b6b4e28e85f1e01
DOFI <74297231+dofi-test@users.noreply.github.com>
2020-11-13 16:00:39 +0800

    Add files via upload

---
+++ myBlocks/blockly/javascript.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['sayhello'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("哩賀!!")';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
